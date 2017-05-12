1aff42ff0fd6806af7e7d2e4c8e259efe2eef763
Teerapat Chaiwachirasak <Teerapat12@users.noreply.github.com>
2017-05-12 07:50:03 +0700

    Add files via upload

---
+++ Keypad/Keypad/Keypad.ino
@@ -0,0 +1,116 @@
+/* Locked Relays
+
+   An Arduino Uno, Keypad, Relays and some LEDs for fun
+
+   Using a password to enable the relays, then selective
+      toggle the relays by key.
+
+   Needed libraries
+   http://arduino.cc/playground/uploads/Code/Keypad.zip
+   http://arduino.cc/playground/uploads/Code/Password.zip
+*/
+#include <Keypad.h>
+#include <Password.h>
+int relay1 = 2;
+int relay2 = 3;
+int relay3 = 4;
+int relay4 = 5;
+int locked = 1;
+int passinput = 0;
+int lockedled = 14;
+int unlockedled = 15;
+long ledflashvar = 0;
+long ledflashtime = 300;
+const byte ROWS = 4;
+const byte COLS = 4;
+char keys[ROWS][COLS] = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
+byte rowPins[ROWS] = {13, 12, 11, 10};
+byte colPins[COLS] = {9, 8, 7, 6};
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+Password password = Password("0000");   // รหัสของการปลดล็อค
+void setup() {
+  //  กำหนดค่าเริ่มต้น และ PIN ต่างๆ
+  Serial.begin(9600);
+  pinMode(relay1, OUTPUT);
+  digitalWrite(relay1, 255);
+  pinMode(relay2, OUTPUT);
+  digitalWrite(relay2, 255);
+  pinMode(relay3, OUTPUT);
+  digitalWrite(relay3, 255);
+  pinMode(relay4, OUTPUT);
+  digitalWrite(relay4, 255);
+  pinMode(lockedled, OUTPUT);
+  digitalWrite(lockedled, 255);
+  pinMode(unlockedled, OUTPUT);
+  digitalWrite(unlockedled, 0);
+}
+void loop() {
+  char key = keypad.getKey();  // รับค่าจาก keypad
+
+
+  // กรณีที่ Lock อยู่  LED สีแดงจะติด  และหากอยู่ระหว่างการใส่รหัส LED สีแดงจะกระพริบ
+  if (locked) {
+    if (passinput) {
+      unsigned long ledcurrentvar = millis();
+      if (ledcurrentvar - ledflashvar > ledflashtime) {
+        ledflashvar = ledcurrentvar;
+        digitalWrite(lockedled, !digitalRead(lockedled));
+      }
+    }
+
+    // ถ้าไม่ได้อยู่ระหว่างการใส่รหัส
+    else {
+      digitalWrite(lockedled, 255);   // LED สีแดงจะติด
+    }
+    digitalWrite(unlockedled, 0);     // LED สีเขียวดับ
+  }
+
+
+  if (key != NO_KEY) {
+    Serial.println(key);   // แสดงค่าที่กดบน Serial monitor
+    password.append(key);  // บันทึกค่ารหัสที่กดและเปรียบเทียบกับรหัสที่ถูกต้อง
+    passinput = 1;         // อยู่ระหว่างการใส่รหัส
+
+    // ถ้ากด '*' ให้เริ่มใส่รหัสใหม่
+    if (key == '*') {
+      password.reset();     // เริ่มต้นใส่รหัสใหม่ตั้งแต่ตัวแรก
+      passinput = 0;        // ไม่อยู่ระหว่างการกดรหัส
+      locked = 1;
+      digitalWrite(relay1, HIGH);   // ตั้งค่า Relay ให้อยู่ในสถานะ Hight
+      digitalWrite(relay2, HIGH);
+      digitalWrite(relay3, HIGH);
+      digitalWrite(relay4, HIGH);
+    }
+
+    if (password.evaluate()) {      //  ถ้าใส่ Password ถูกต้องให้ปลดล็อค
+      locked = !locked;
+      password.reset();             // เริ่มต้นใส่รหัสใหม่ตั้งแต่ตัวแรก
+      passinput = 0;
+    }
+
+    //  ถ้าปลดล๊อคแล้วให้ไฟสีเขียวติด  สีแดงดับ  และสามารถควบคุมการเปิดปิดของ Relay ได้
+    if (!locked) {
+      passinput = 0;
+      digitalWrite(lockedled, 0);
+      digitalWrite(unlockedled, 255);
+
+      switch (key) {                //  และเลือก Relay ที่ต้องการควบคุม
+        case 'A':
+          digitalWrite(relay1, !digitalRead(relay1));
+          break;
+        case 'B':
+          digitalWrite(relay2, !digitalRead(relay2));
+          break;
+        case 'C':
+          digitalWrite(relay3, !digitalRead(relay3));
+          break;
+        case 'D':
+          digitalWrite(relay4, !digitalRead(relay4));
+          break;
+      }
+
+
+      password.reset();              // เริ่มต้นใส่รหัสใหม่ตั้งแต่ตัวแรก
+    }
+  }
+}
---
+++ index.ino
@@ -0,0 +1,527 @@
+/*4x4 Matrix Keypad connected to Arduino
+This code prints the key pressed on the keypad to the serial port*/
+
+#include <Keypad.h>
+#include <Wire.h> 
+#include <EEPROM.h>
+#include "FPS_GT511C3.h"
+#include "SoftwareSerial.h"
+
+#include <LiquidCrystal_I2C.h> //ประกาศ Library ของจอ I2C
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+const byte numRows= 4; //number of rows on the keypad
+const byte numCols= 4; //number of columns on the keypad
+
+//keymap defines the key pressed according to the row and columns just as appears on the keypad
+char keymap[numRows][numCols]= 
+{
+{'1', '2', '3', 'A'}, 
+{'4', '5', '6', 'B'}, 
+{'7', '8', '9', 'C'},
+{'*', '0', '#', 'D'}
+};
+
+ int menuNum = 6;
+ String foodMenuName[] = {"Thai","Japanese","French","Italian","Texas","Mixed"};
+ int foodMenuPrice[] ={50,120,75,83,79,150};
+
+//Code that shows the the keypad connections to the arduino terminals
+byte rowPins[numRows] = {13,12,11,10}; //Rows 0 to 3
+byte colPins[numCols]= {9,8,7,6}; //Columns 0 to 3
+
+//initializes an instance of the Keypad class
+Keypad myKeypad= Keypad(makeKeymap(keymap), rowPins, colPins, numRows, numCols);
+
+FPS_GT511C3 fps(4, 5);
+
+
+void setup()
+{
+  Serial.begin(9600);
+  lcd.begin();
+  // Print a message to the LCD.
+  printLine("---Finger Pay---",0);
+  printLine("Loading.",1);
+
+  
+  //fps.UseSerialDebug = true; // so you can see the messages in the serial debug screen
+  fps.Open();
+  lcd.print(".");
+  fps.SetLED(true);
+  
+  lcd.print(".");
+//  fps.DeleteAll();
+  printLine("(Enroll) D P",1);
+
+}
+
+//If key is pressed, this key is stored in 'keypressed' variable
+//If key is not equal to 'NO_KEY', then this key is printed out
+//if count=17, then count is reset back to 0 (this means no key is pressed during the whole keypad scan process
+void printLine(String message,int  l){
+  clearLine(l);
+  // Now write a message to line 1
+  lcd.setCursor (0, l);
+  lcd.print(message);
+}
+
+void clearLine(int l){
+  lcd.setCursor (0, l);
+   for (int i = 0; i < 16; ++i)
+  {
+    lcd.write(' ');
+  }
+  
+  lcd.setCursor(0,l);
+}
+
+char mainMenu(){
+  char keypressed = myKeypad.getKey();
+  char selectedMenu='A';
+  printLine("---Finger Pay---",0);
+  printLine("(Enroll) A P",1);
+  do{
+      
+      keypressed = myKeypad.getKey();
+      if (keypressed != NO_KEY)
+      {
+        switch(keypressed){
+          case 'A': 
+            selectedMenu = 'A';
+            printLine("(Enroll) A P",1);
+            break;
+          case 'B':
+            selectedMenu = 'B';
+            printLine("E (Account) P",1);
+            break;
+          case 'C':
+            selectedMenu = 'C';
+            printLine("E A (Payment)",1);
+            break;
+        }
+        Serial.print(keypressed);
+        
+      }
+  } while (keypressed!='D');
+
+  return selectedMenu;
+}
+
+void printAccoutBalance(int id){
+  printLine("-Balance: ",0);
+  lcd.print(EEPROM.read(id));
+  lcd.print("--");
+  return;
+}
+
+void account(){ 
+  printLine("----Account----",0);
+   char keypressed = myKeypad.getKey();
+  char selectedMenu='A';
+  printLine("(Balance) D W",1);
+  do{
+      
+      keypressed = myKeypad.getKey();
+      if (keypressed != NO_KEY)
+      {
+        switch(keypressed){
+          case 'A': 
+            selectedMenu = 'A';
+            printLine("(Balance) D W",1);
+            break;
+          case 'B':
+            selectedMenu = 'B';
+            printLine("B (Deposit) W",1);
+            break;
+          case 'C':
+            selectedMenu = 'C';
+            printLine("B D (Withdraw)",1);
+            break;
+        }
+        Serial.print(keypressed);
+        
+      }
+  } while (keypressed!='D');
+Serial.print(selectedMenu);
+  int id;
+
+   while(true){
+        printLine("Scan finger",1);
+        while(fps.IsPressFinger()==false){ delay(100); 
+         char keypressed = myKeypad.getKey();
+      if (keypressed == '*'){
+        return;
+      }}
+        fps.CaptureFinger(false);
+      
+       id = fps.Identify1_N();
+   
+  switch(selectedMenu){
+    case 'A':
+      //Check Balance
+      // Identify fingerprint test
+
+     
+        if (id <200)
+        {
+          Serial.print("Verified ID:");
+          Serial.println(id);
+         
+          printLine("Balance(",1);
+          lcd.print(id);
+          lcd.print("): ");
+          lcd.print(EEPROM.read(id));
+          while(myKeypad.getKey()!='D'){delay(100);}
+          return;
+        }
+        else
+        {
+          Serial.println("Finger not found");
+          printLine("Finger not found",1);
+          delay(2000);
+        }
+      
+    
+      break;
+    case 'B':
+        if (id <200)
+        {
+          Serial.print("Verified ID:");
+          Serial.println(id);
+          printAccoutBalance(id);
+          printLine("Deposit(",1);
+          lcd.print(id);
+          lcd.print("): ");
+          int startPosition = 11+String(id).length();
+          int lcdPosition = startPosition;
+           char keypressed = myKeypad.getKey();  
+                  String amount = "";
+                do{
+                    
+                    keypressed = myKeypad.getKey();
+                    if (keypressed != NO_KEY && isDigit(keypressed) &&lcdPosition<16)
+                    {
+                      amount+=keypressed;
+                      lcdPosition+=1;
+                      lcd.write(keypressed);
+                      Serial.print(keypressed);
+                      
+                    }
+                    if(keypressed=='#' &&lcdPosition>startPosition){
+                      amount.remove(amount.length()-1,1);
+                      lcdPosition-=1;
+                     lcd.setCursor(lcdPosition,1);
+                      lcd.print(" ");
+                      lcd.setCursor(lcdPosition,1);
+                    }
+                } while (keypressed!='D'|| amount=="");
+                int newAmount = amount.toInt()+EEPROM.read(id);
+                if(newAmount<0) {printLine("Exceed maximum",1); delay(2000);}
+                else {
+                  printLine("New balance: ",1);
+                  lcd.print(newAmount);
+                  EEPROM.write(id,newAmount);
+                  delay(4000);
+                  return;
+                  }
+  
+        break;
+        }
+        else
+        {
+          Serial.println("Finger not found");
+          printLine("Finger not found",1);
+          delay(2000);
+        }
+      //Deposit
+      break;
+    case 'C':
+      //Withdraw
+       if (id <200)
+        {
+          Serial.print("Verified ID:");
+          Serial.println(id);
+          printAccoutBalance(id);
+          printLine("Withdraw(",1);
+          lcd.print(id);
+          lcd.print("): ");
+          int startPosition = 12+String(id).length();
+          int lcdPosition = startPosition;
+           char keypressed = myKeypad.getKey();  
+                  String amount = "";
+                do{
+                    
+                    keypressed = myKeypad.getKey();
+                    if (keypressed != NO_KEY && isDigit(keypressed) &&lcdPosition<16)
+                    {
+                      amount+=keypressed;
+                      lcdPosition+=1;
+                      lcd.write(keypressed);
+                      Serial.print(keypressed);
+                      
+                    }
+                    if(keypressed=='#' &&lcdPosition>startPosition){
+                      amount.remove(amount.length()-1,1);
+                      lcdPosition-=1;
+                     lcd.setCursor(lcdPosition,1);
+                      lcd.print(" ");
+                      lcd.setCursor(lcdPosition,1);
+                    }
+                } while (keypressed!='D'|| amount=="");
+                int newAmount = EEPROM.read(id)-amount.toInt();
+                if(newAmount>250) {printLine("Not enough money",1); delay(2000);}
+                else {
+                  printLine("New balance: ",1);
+                  lcd.print(newAmount);
+                  EEPROM.write(id,newAmount);
+                  delay(4000);
+                  return;
+                  }
+  
+        break;
+        }
+        else
+        {
+          Serial.println("Finger not found");
+          printLine("Finger not found",1);
+          delay(2000);
+        }
+      break;
+  }
+
+   }
+
+  delay(500);
+  return;
+}
+
+void enroll(){
+  Enroll();
+}
+
+void pay(){
+printMenu(0,true);
+  int curMenu = 0;
+  char keypressed = myKeypad.getKey();
+  boolean mode = true; //true = Lower, false=UPPER
+   do{
+                    
+      keypressed = myKeypad.getKey();
+      if (keypressed != NO_KEY)
+      {
+        Serial.println(menuNum);
+        if(mode==true){
+          if(keypressed=='B'&& curMenu<  menuNum-1){
+            curMenu+=1;
+            printMenu(curMenu,mode);
+          }
+          else if(keypressed=='A'&& curMenu>0){            
+            mode = false;
+            curMenu-=1;
+            printMenu(curMenu,mode);
+          }
+        }
+        else if(mode==false){
+          if(keypressed=='B'&& curMenu<menuNum-1){
+            curMenu+=1;
+            mode = true;
+            printMenu(curMenu,mode);
+
+          }
+          else if(keypressed=='A'&& curMenu>0){
+            curMenu-=1;
+            printMenu(curMenu,mode);
+          }
+        }
+        
+      }
+  } while (keypressed!='D');
+
+//  Confirm withFinger.
+int id2;
+  while(true){
+        printLine("-- ",0);
+        lcd.print(foodMenuName[curMenu]);
+        lcd.print("(");
+        lcd.print(foodMenuPrice[curMenu]);
+        lcd.print(")--");
+        printLine("Scan finger2pay",1);
+        while(fps.IsPressFinger()==false){ delay(100); 
+         char keypressed = myKeypad.getKey();
+      if (keypressed == '*'){
+        return;
+      }}
+        fps.CaptureFinger(false);
+      
+       id2 = fps.Identify1_N();
+
+
+         if (id2 <200)
+        {
+          Serial.print("Verified ID:");
+          Serial.println(id2);
+                int newAmount = EEPROM.read(id2)-foodMenuPrice[curMenu];
+                if(newAmount<0) {printLine("Not enough money",1); delay(2000);}
+                else {
+                  printLine("New balance: ",1);
+                  lcd.print(newAmount);
+                  EEPROM.write(id2,newAmount);
+                  delay(4000);
+                  return;
+                  }
+  
+        break;
+        }
+        else
+        {
+          Serial.println("Finger not found");
+          printLine("Finger not found",1);
+          delay(2000);
+        }
+  }
+
+//Check money.
+//Done.
+
+
+}
+
+void printMenu(int curMenu,boolean mode){
+  if(mode==true){
+    if(curMenu>0) {printLine(foodMenuName[curMenu-1],0); lcd.print("("); lcd.print(foodMenuPrice[curMenu-1]); lcd.print(")");}
+    else printLine("Select Menu",0);
+    printLine(foodMenuName[curMenu],1);
+    lcd.print("("); lcd.print(foodMenuPrice[curMenu]); lcd.print(")");
+    lcd.print("<");
+  }
+  else{
+     if(curMenu>=0) {printLine(foodMenuName[curMenu],0); lcd.print("("); lcd.print(foodMenuPrice[curMenu]); lcd.print(")");}
+    else {printLine("Select Menu",0);}
+
+    lcd.print("<");
+    printLine(foodMenuName[curMenu+1],1);
+    lcd.print("("); lcd.print(foodMenuPrice[curMenu+1]); lcd.print(")");
+  }
+}
+
+void loop(){
+  char selectedMenu = mainMenu();
+
+  switch(selectedMenu){
+    case 'A':
+      enroll();
+      break;
+    case 'B':
+      account();
+      break;
+    case 'C':
+      pay();
+      break;
+  }
+  Serial.println(selectedMenu);
+  lcd.clear();
+  delay(500);
+}
+
+void Enroll()
+{
+  // Enroll test
+  
+  // find open enroll id
+  int enrollid = 0;
+  bool usedid = true;
+  while (usedid == true)
+  {
+    usedid = fps.CheckEnrolled(enrollid);
+    if (usedid==true) enrollid++;
+  }
+  fps.EnrollStart(enrollid);
+
+  while(true){
+  // enroll
+  Serial.print("Press finger to Enroll #");
+  clearLine(0);
+  lcd.print("Enrolling #");
+  lcd.print(enrollid);
+  clearLine(1);
+  lcd.print("Scan finger");
+  Serial.println(enrollid);
+  while(fps.IsPressFinger() == false){
+    delay(100);
+      char keypressed = myKeypad.getKey();
+      if (keypressed == '*'){
+        return;
+      }
+    };
+  clearLine(0);
+  lcd.print("Enrolling #");
+  lcd.print(enrollid);
+  printLine("Remove finger",1);
+  bool bret = fps.CaptureFinger(true);
+  int iret = 0;
+  if (bret != false)
+  {
+    Serial.println("Remove finger");
+    fps.Enroll1(); 
+    while(fps.IsPressFinger() == true) delay(100);
+    Serial.println("Press same finger again");
+    printLine("Press again (2)",1);
+    while(fps.IsPressFinger() == false){
+    delay(100);
+      char keypressed = myKeypad.getKey();
+      if (keypressed == '*'){
+        return;
+      }
+    };
+    bret = fps.CaptureFinger(true);
+    if (bret != false)
+    {
+      Serial.println("Remove finger");
+      printLine("Remove finger",1);
+      fps.Enroll2();
+      while(fps.IsPressFinger() == true) delay(100);
+      Serial.println("Press same finger yet again");
+      printLine("Press again (3)",1);
+      while(fps.IsPressFinger() == false){
+    delay(100);
+      char keypressed = myKeypad.getKey();
+      if (keypressed == '*'){
+        return;
+      }
+    };
+      bret = fps.CaptureFinger(true);
+      if (bret != false)
+      {
+        Serial.println("Remove finger");
+        printLine("Remove finger",1);
+        iret = fps.Enroll3();
+        if (iret == 0)
+        {
+          Serial.println("Enrolling Successfull");
+          printLine("Enrolling Successfull",0);
+          printLine("Going back to main menu",1);
+          EEPROM.write(enrollid,0);
+          delay(4000);
+          break;
+        }
+        else
+        {
+          Serial.print("Enrolling Failed with error code:");
+          clearLine(1);
+          lcd.write("Failed code:");
+          lcd.print(iret);
+          delay(4000);
+          Serial.println(iret);
+        }
+      }
+      else  {Serial.println("Failed to capture third finger");  printLine("Failed 3rd finger",1); delay(4000);}
+    }
+    else {Serial.println("Failed to capture second finger"); printLine("Failed 2nd finger",1); delay(4000);};
+  }
+  else {Serial.println("Failed to capture first finger"); printLine("Failed 1st finger",1); delay(4000);}
+  }
+  
+}
